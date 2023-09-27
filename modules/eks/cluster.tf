8dfe95cd4fadb04df2835ac6cb7f4dadda43e4d8
Mohamed Eldeeb <81789671+0xZe@users.noreply.github.com>
2023-09-27 16:20:23 +0300

    Add cluster.tf

---
+++ modules/eks/cluster.tf
@@ -0,0 +1,22 @@
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = "eks-cluster"
+  role_arn = aws_iam_role.eks-role.arn
+
+
+  version = "1.23"
+
+  vpc_config {
+    subnet_ids = var.private_subnets_id
+    security_group_ids = [aws_security_group.eks-cluster-sg.id]
+    endpoint_private_access = true
+    endpoint_public_access  = false
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role.eks-role,
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
+  ]
+}
