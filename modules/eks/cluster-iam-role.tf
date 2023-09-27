4d5d654343295a6274ab0daab1324792fb4ffdb3
Mohamed Eldeeb <81789671+0xZe@users.noreply.github.com>
2023-09-28 00:29:51 +0300

    Update cluster-iam-role.tf

--- modules/eks/cluster-iam-role.tf
+++ modules/eks/cluster-iam-role.tf
@@ -1,29 +1,23 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
+#iam_role
 
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
+resource "aws_iam_role" "EKSClusterRole" {
+  name = "EKSClusterRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
-resource "aws_iam_role" "eks-role" {
-  name               = "tf-eks-cluster-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-role.name
-}
-
-# Optionally, enable Security Groups for Pods
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
-resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks-role.name
+  role       = aws_iam_role.EKSClusterRole.name
 }
