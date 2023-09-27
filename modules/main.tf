d27ec651105c43977452f05f0592a4cff77d9cdf
Mohamed Eldeeb <81789671+0xZe@users.noreply.github.com>
2023-09-27 17:04:10 +0300

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,5 +1,5 @@
 module "vpc-network" {
-  source                        = "./modules/vpc-network"
+  source                        = "./vpc-network"
   vpc_cidr                      = var.vpc_cidr
   public_subnets_cidr           = var.public_subnets_cidr
   private_subnets_cidr          = var.private_subnets_cidr
@@ -7,16 +7,16 @@ module "vpc-network" {
 
 }
 
-module "jump-host" {
-  source                       = "./modules/bastion"
+module "bastion" {
+  source                       = "./bastion"
 
   appvpc_id                  = module.vpc-network.appvpc_id
   public_subnets_id          = [module.vpc-network.public_subnet_id, module.vpc-network.public_subnet2_id]
 
 }
 
-module "eks-cluster" {
-  source                 = "./modules/eks"
+module "eks" {
+  source                 = "./eks"
   appvpc_id                  = module.vpc-network.appvpc_id
   private_subnets_id = [module.vpc-network.private_subnet_id, module.vpc-network.private_subnet2_id]
 
